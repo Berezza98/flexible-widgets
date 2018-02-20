141d94a42b8ded9ef3e68b39e527684b68c09262
roman <berezdecky98@gmail.com>
2018-02-20 18:55:29 +0200

    search box added

--- src/components/columns/SecondColumn.vue
+++ src/components/columns/SecondColumn.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="secondColumn">
-      <router-view></router-view>
+      <div class="search">
+          <input v-model="searchingText" type="text" class="searchBox">
+      </div>
+      <div class="container">
+          <router-view></router-view>
+      </div>
   </div>
 </template>
 
 <script>
     export default{
-        
+        data(){
+            return{
+                searchingText: ""
+            }
+        }
     }
 </script>
 
@@ -15,6 +24,25 @@
         height: 100%;
         width: 25%;
         background: rgb(199, 223, 247);
+    }
+
+    .search{
+        height: 100px;
+        width: 100%;
+        background: rgb(104, 98, 90);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .searchBox{
+        width: 95%;
+        height: 40px;
+        font-size: 30px
+    }
+
+    .container{
+        height: calc(100% - 100px);
         overflow-y: scroll;
     }
 </style>
